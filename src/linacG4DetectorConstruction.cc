fb891a176d1faa39e4d12d7257d219d97eff39bd
Sergey Dubrovin <dubrovin.sa@rambler.ru>
2022-11-02 12:38:58 +0300

    genericpolycon и polycone

--- src/linacG4DetectorConstruction.cc
+++ src/linacG4DetectorConstruction.cc
@@ -7,7 +7,9 @@
 #include "G4Orb.hh"
 #include "G4Sphere.hh"
 #include "G4Trd.hh"
+#include "G4Tubs.hh"
 #include "G4Polycone.hh"
+#include "G4GenericPolycone.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
@@ -22,87 +24,94 @@ DetectorConstruction::~DetectorConstruction()
 
 G4VPhysicalVolume *DetectorConstruction::Construct()
 {
+    // Get nist material manager
     G4NistManager *nist = G4NistManager::Instance();
 
-    G4double env_sizeXY = 100 * cm, env_sizeZ = 100 * cm;
+    // Envelope parameters
+    //
+    G4double env_sizeXY = 20 * cm, env_sizeZ = 30 * cm;
     G4Material *env_mat = nist->FindOrBuildMaterial("G4_WATER");
 
+    // Option to switch on/off checking of volumes overlaps
+    //
     G4bool checkOverlaps = true;
 
-    G4double world_sizeXY = env_sizeXY;
-    G4double world_sizeZ = env_sizeZ;
+    //
+    // World
+    //
+    G4double world_sizeXY = 1.2 * env_sizeXY;
+    G4double world_sizeZ = 1.2 * env_sizeZ;
     G4Material *world_mat = nist->FindOrBuildMaterial("G4_AIR");
 
     G4Box *solidWorld =
-        new G4Box("World",
-                  0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ);
+        new G4Box("World",                                                    // its name
+                  0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ); // its size
 
     G4LogicalVolume *logicWorld =
-        new G4LogicalVolume(solidWorld,
-                            world_mat,
-                            "World");
+        new G4LogicalVolume(solidWorld, // its solid
+                            world_mat,  // its material
+                            "World");   // its name
 
     G4VPhysicalVolume *physWorld =
-        new G4PVPlacement(0,
-                          G4ThreeVector(),
-                          logicWorld,
-                          "World",
-                          0,
-                          false,
-                          0,
-                          checkOverlaps);
+        new G4PVPlacement(0,               // no rotation
+                          G4ThreeVector(), // at (0,0,0)
+                          logicWorld,      // its logical volume
+                          "World",         // its name
+                          0,               // its mother  volume
+                          false,           // no boolean operation
+                          0,               // copy number
+                          checkOverlaps);  // overlaps checking
 
+    //
+    // Envelope
+    //
     G4Box *solidEnv =
-        new G4Box("Envelope",
-                  0.5 * env_sizeXY, 0.5 * env_sizeXY, 0.5 * env_sizeZ);
+        new G4Box("Envelope",                                           // its name
+                  0.5 * env_sizeXY, 0.5 * env_sizeXY, 0.5 * env_sizeZ); // its size
 
     G4LogicalVolume *logicEnv =
-        new G4LogicalVolume(solidEnv,
-                            env_mat,
-                            "Envelope");
+        new G4LogicalVolume(solidEnv,    // its solid
+                            env_mat,     // its material
+                            "Envelope"); // its name
 
-    new G4PVPlacement(0,
-                      G4ThreeVector(),
-                      logicEnv,
-                      "Envelope",
-                      logicWorld,
-                      false,
-                      0,
-                      checkOverlaps);
+    new G4PVPlacement(0,               // no rotation
+                      G4ThreeVector(), // at (0,0,0)
+                      logicEnv,        // its logical volume
+                      "Envelope",      // its name
+                      logicWorld,      // its mother  volume
+                      false,           // no boolean operation
+                      0,               // copy number
+                      checkOverlaps);  // overlaps checking
 
     //
     // Flattering filter
     //
-    G4Material *FF_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
+    G4Material *FF_mat = nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"); // !!!
     G4ThreeVector posFF = G4ThreeVector(0, 0, 0);
-    G4double FF_phimin = 0.0 * deg, FF_phimax = 360.0 * deg;
-    G4int numZP = 3;
-    
-    G4double zInner[] = {0.344*cm, 0.331*cm, 0.025*cm};
 
-    G4double rOuter[] = {0.0*cm, 0.047*cm, 0.093*cm}; 
+    // Polycon section shape
+    // G4int nmbRZ = 41;
+    G4int nmbRZ = 10;
 
-    G4Polycone *solidFF = new G4Polycone(
-        "flattering filter",
-        FF_phimin,
-        FF_phimax,
-        numZP,
-        rOuter,
-        zInner);
+    G4double r[] = {1.3650 * cm, 1.3630 * cm, 1.3600 * cm, 1.3550 * cm, 1.3490 * cm,
+                    1.3400 * cm, 1.3320 * cm, 1.3200 * cm, 1.3100 * cm, 1.2720 * cm};
 
-    G4LogicalVolume* logicFF =
-    new G4LogicalVolume(solidFF,         //its solid
-                        FF_mat,          //its material
-                        "flattering filter");           //its name
+    G4double z[] = {0. * cm, 0.012 * cm, 0.033 * cm, 0.040 * cm, 0.050 * cm,
+                    0.068 * cm, 0.075 * cm, 0.068 * cm, 0.075 * cm, 0.090 * cm};
 
-    new G4PVPlacement(0,                       //no rotation
-                    posFF,                    //at position
-                    logicFF,             //its logical volume
-                    "flattering filter",                //its name
-                    logicFF,                //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
+    // G4Polycone *solidFF = new G4Polycone("FF", 0.0 * deg, 360.0 * deg, nmbRZ, r, z);
+    G4GenericPolycone *solidFF = new G4GenericPolycone("FF", 0.0 * deg, 359.0 * deg, nmbRZ, r, z);
+
+    G4LogicalVolume *logicFF = new G4LogicalVolume(solidFF, FF_mat, "FF");
+
+    new G4PVPlacement(0,
+                      posFF,
+                      logicFF,
+                      "FF",
+                      logicFF,
+                      false,
+                      0,
+                      checkOverlaps);
 
     return physWorld;
 }
